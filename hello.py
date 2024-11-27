65111ff09f287673d8d87a96c4780178b850faf6
yukiyamamuro <yuki.sumaho@gmail.com>
2024-11-27 09:41:20 +0900

    chore: parallel chain

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_google_community import GoogleDriveLoader
+from langchain_core.runnables import RunnableLambda, RunnableParallel
 from langchain_openai import ChatOpenAI
 from pydantic import BaseModel, Field
 
-model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+model = ChatOpenAI(model="gpt-4o-mini", temperature=1)
 
 class Recipe(BaseModel):  # noqa: D101
     ingredients: list[str] = Field(description="材料")
@@ -28,7 +28,27 @@ prompt_with_format_instructions = prompt.partial(
     format_instructions=format_instructions
 )
 
-chain = prompt_with_format_instructions | model | output_parser
-recipe = chain.invoke({"dish":"カレー"})
-print(recipe)
+def printerer(text: str) -> str:
+    print(text)
+    return text
+
+base_chain = prompt_with_format_instructions | RunnableLambda(printerer) | model | output_parser
+
+synthesize_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "レシピの似た点があれば教えてください"),
+        ("human", "レシピ1: {rice}\n\nレシピ2: {nann}"),
+    ]
+)
+
+synthesize_chain = (RunnableParallel({
+    "rice": base_chain,
+    "nann": base_chain
+    })
+    | synthesize_prompt
+)
+
+result=synthesize_chain.invoke({"dish": "チキンカレー"})
+print("===result===")
+print(result)
 
